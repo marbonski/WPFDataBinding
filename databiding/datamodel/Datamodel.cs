25187803ec32d586358a44081a402086d58d69fd
Piotr Kwiatkowski <piotr.kwiatkowski@pb.com>
2018-08-27 15:32:34 +0200

    finished bidings

---
+++ databiding/datamodel/Datamodel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace databiding.datamodel
+{    
+    public class Person : INotifyPropertyChanged
+    {
+        string _firstName;
+        string _lastName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                this.OnPropertyChanged("FirstName");
+                this.OnPropertyChanged("FullName");
+            }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                this.OnPropertyChanged("LastName");
+                this.OnPropertyChanged("FullName");
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return String.Format("{0}, {1}", this.LastName, this.FirstName);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged( string propName )
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+
+    }
+   
+}
---
+++ databiding/view/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using databiding.datamodel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace databiding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+   // 
+
+    public partial class MainWindow : Window
+    {
+        private Person _person;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //ManyallyMoveData();
+            //BindInCode();
+            BindInXaml();
+        }
+
+        private void ManyallyMoveData()
+        {
+            _person = new Person
+            {
+                FirstName = "Piotr",
+                LastName = "Kwiatkowski"
+            };
+
+            this.firstNameTextBox.Text = _person.FirstName;
+            this.lastNameTextBox.Text = _person.LastName;
+            this.firstNameTextBox.Text = _person.FullName;
+
+            this.firstNameTextBox.TextChanged += firstNameTextBox_TextChanged;
+            this.lastNameTextBox.TextChanged += lastNameTextBox_TextChanged;
+        }
+
+        void lastNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            _person.LastName = this.lastNameTextBox.Text;
+            this.fullNameTextBlock.Text = _person.FullName;
+        }
+
+        void firstNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            _person.FirstName = this.firstNameTextBox.Text;
+            this.fullNameTextBlock.Text = _person.FullName;
+        }
+
+        private void BindInCode()
+        {
+            var person = new Person
+            {
+                FirstName = "John",
+                LastName = "Julits"
+            };
+
+            Binding b = new Binding();
+            b.Source = person;
+            b.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            b.Path = new PropertyPath("FirstName");
+            this.firstNameTextBox.SetBinding(TextBox.TextProperty, b);
+
+            b = new Binding();
+            b.Source = person;
+            b.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            b.Path = new PropertyPath("LastName");
+            this.lastNameTextBox.SetBinding(TextBox.TextProperty, b);
+
+            b = new Binding();
+            b.Source = person;
+            b.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            b.Path = new PropertyPath("FullName");
+            this.fullNameTextBlock.SetBinding(TextBlock.TextProperty, b);
+        }        
+
+        private void BindInXaml()
+        {
+            base.DataContext = new Person
+            {
+                FirstName = "Jacek",
+                LastName = "Trefon"
+            };
+        }
+    }
+}
